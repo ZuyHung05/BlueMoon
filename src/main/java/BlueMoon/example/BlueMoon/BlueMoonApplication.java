a4f4ed6e2ccd0a4be9f64406908ff5e37a9a41ac
ZuyHung <quyqlht@gmail.com>
2025-10-20 20:49:20 +0700

    Initial commit of BlueMoon project code

---
+++ src/main/java/BlueMoon/example/BlueMoon/BlueMoonApplication.java
@@ -0,0 +1,13 @@
+package BlueMoon.example.BlueMoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueMoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BlueMoon/example/BlueMoon/BlueMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package BlueMoon.example.BlueMoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueMoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
